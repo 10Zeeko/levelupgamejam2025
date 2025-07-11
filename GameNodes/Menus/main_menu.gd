2e3b1664df68e30d6e6aa18762e926c9338f5714
Zeeko <57009593+10Zeeko@users.noreply.github.com>
2025-07-12 01:21:44 +0200

    Cambios main menu

--- GameNodes/Menus/main_menu.gd
+++ GameNodes/Menus/main_menu.gd
@@ -1,6 +1,19 @@
 extends Control
 
 @export var next_level : PackedScene
+@onready var world_environment: WorldEnvironment = $WorldEnvironment
+
+var base_intensity := 0.75
+var flicker_timer := 0.0
+var flicker_interval := 0.05
+
+func _process(delta: float) -> void:
+	flicker_timer -= delta
+	if flicker_timer <= 0.0:
+		flicker_timer = randf_range(0.03, 0.45)
+		var flicker = randf_range(-0.25, 0.25)
+		var new_intensity = clamp(base_intensity + flicker, 0.0, 1.0)
+		world_environment.environment.glow_intensity = new_intensity
 
 func _on_button_pressed() -> void:
 	call_deferred("_change_scene")
